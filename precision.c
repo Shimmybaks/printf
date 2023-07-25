88e19dde3c6b43b9ab66795931f1dd4d8d3c8053
DamilareOy <oyaniyidamilare57@gmail.com>
2023-07-25 16:50:37 -0700

    Precision

--- precision.c
+++ precision.c
@@ -27,8 +27,8 @@ int get_precision(const char *format, int *i, va_list list)
 		}
 		else if (format[curr_i] == '*')
 		{
-			curr_i++;
 			precision = va_arg(list, int);
+			curr_i++;
 			break;
 		}
 		else
